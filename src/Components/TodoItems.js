b127dec53700ba7bdbc176ab27eee4926041074e
reedm26 <reedm26@yahoo.com>
2020-08-26 12:48:04 -0600

    have the strikeout working with toggle

--- src/Components/TodoItems.js
+++ src/Components/TodoItems.js
@@ -1,10 +1,31 @@
 import React, { Component } from "react";
+import propTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import { List, ListItem, ListItemText } from "@material-ui/core";
 
 export class TodoItems extends Component {
+  getStyle = () => {
+    if (this.props.todo.completed)
+      return {
+        background: "#f4f4f4",
+        padding: "10px",
+        borderBottom: "1px #ccc dotted",
+        textDecoration: this.props.todo.completed ? "line-through" : "none",
+      };
+  };
+
   render() {
+    const { id, title } = this.props.todo;
     return (
-      <div>
-        <p> Hello </p>
+      <div style={this.getStyle()}>
+        <p>
+          {" "}
+          <input
+            type="checkbox"
+            onChange={this.props.markComplete.bind(this, id)}
+          />{" "}
+          {title}
+        </p>
       </div>
     );
   }
--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
 import TodoItems from "./TodoItems";
+import propTypes from "prop-types";
 class Todos extends Component {
   render() {
-    return this.props.todos.map((todo) => <TodoItems todo={todo} />);
+    return this.props.todos.map((todo) => (
+      <TodoItems
+        key={todo.id}
+        todo={todo}
+        markComplete={this.props.markComplete}
+      />
+    ));
   }
+  //PropTypes
+  Todos;
+  propTypes = {
+    todos: propTypes.array.isRequired,
+  };
 }
 
 export default Todos;
